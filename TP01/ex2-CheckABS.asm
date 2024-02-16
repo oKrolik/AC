31153df4f691e1bd51ef614e4ddb5754db8d5dfd
Guilherme Coelho <gcoelho2002@gmail.com>
2024-02-16 10:53:26 +0000

    fix texts and removed last empty line

--- TP01/ex2-CheckABS.asm
+++ TP01/ex2-CheckABS.asm
@@ -3,18 +3,17 @@
 
 # a0 = valor "limite" X
 # a1 = tamanho do vetor
-# a2 = endereÁo do vetor
+# a2 = endere√ßo do vetor
      
 CheckABS:
-       li   t0, 0        # Contador de ocorrÍncias
-iter:  lw   t1, 0(a2)    # LÍ elemento do vetor
+       li   t0, 0        # Contador de ocorr√™ncias
+iter:  lw   t1, 0(a2)    # L√™ elemento do vetor
        bgtu t1, zero, pos
        neg  t1, t1       # Valor absoluto do elemento lido
 pos:   bleu t1, a0, prox
-       sw   zero, 0(a2)  # Como |elemento|>a0 ent„o elemento torna-se 0
-prox:  addi a2, a2, 4    # Atualiza endereÁo   
-       addi a1, a1, -1   # Atualiza iteraÁıes em falta
+       sw   zero, 0(a2)  # Como |elemento|>a0 ent√£o elemento torna-se 0
+prox:  addi a2, a2, 4    # Atualiza endere√ßo   
+       addi a1, a1, -1   # Atualiza itera√ß√µes em falta
        bne  a1, zero, iter 
        mv   a0, t0       # Coloca resultado em a0
        ret
-       
